5d099cc042a0fd6cb9a23022f8a62fdfbd474de7
amanjhurani <amanjhurani5@gmail.com>
2022-10-09 11:19:20 +0530

    fix: ingredients price

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -9,10 +9,10 @@ export const ingredientArray = [
 ];
 
 export const defaultIngredientsPrice = {
-  Cheese: 1 * 5,
-  Salad: 0.5 * 5,
-  Bacon: 0.75 * 5,
-  Meat: 2 * 5,
+  Cheese: 1 * 10,
+  Salad: 0.10 * 10,
+  Bacon: 0.710 * 10,
+  Meat: 2 * 10,
 };
 
 export const defaultAlertText: string = "This is a success message";
